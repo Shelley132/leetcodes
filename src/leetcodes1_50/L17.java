cbe3bec91b98c604d6e891c6c2d046a1f7d9317d
Shelley132 <liulijuan132@qq.com>
2017-08-01 18:49:46 +0800

    L17

--- src/leetcodes1_50/L17.java
+++ src/leetcodes1_50/L17.java
@@ -59,7 +59,25 @@ public class L17 {
 	
 		return list;
 	}
-	
+	//递归做法
+	private static final String[] KEYS = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    
+	public List<String> letterCombinations3(String digits) {
+		List<String> ret = new LinkedList<String>();
+		combination("", digits, 0, ret);
+		return ret;
+	}
+
+	private void combination(String prefix, String digits, int offset, List<String> ret) {
+		if (offset >= digits.length()) {
+			ret.add(prefix);
+			return;
+		}
+		String letters = KEYS[(digits.charAt(offset) - '0')];
+		for (int i = 0; i < letters.length(); i++) {
+			combination(prefix + letters.charAt(i), digits, offset + 1, ret);
+		}
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
