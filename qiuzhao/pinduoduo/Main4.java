9c5688ce7b7c0c751813226542c103026cb88992
Shelley132 <liulijuan132@qq.com>
2017-08-05 14:10:35 +0800

    拼多多第4题

--- qiuzhao/pinduoduo/Main4.java
+++ qiuzhao/pinduoduo/Main4.java
@@ -1,110 +1,108 @@
 package pinduoduo;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
- * @author JUANJUAN
- * @version 2017年8月1日下午9:14:08
+ * @author LLJ
+ * @version 2017年8月2日上午10:40:57
 */
+class Node{
+	int x;
+	int y;
+	int cnt;
+	int sta;
+	public Node(){
+		this.cnt =0;
+		this.sta =0;
+	}
+	public String toString(){
+		return x+"/"+y;
+	}
+}
+
 public class Main4 {
-	 //设置最大值  
-    int MAX=999;  
-    //顶点总个数  
-    int total;  
-    //邻接矩阵表示距离  
-    int[][] matrix;  
-    //结点相关信息  
-    String[] nodes;  
-    //存储最后结果  
-    int[] dis;  
-    //标记结点  
-    int[] mark;  
-  
-   private void dijkstra1(int s)  
-   {  
-       for(int i=0;i<total;i++ )  
-           mark[i]=0;  
-       mark[s]=1;  
-       for(int i=0;i<total;i++)  
-       {  
-           dis[i]=matrix[s][i];  
-       }  
-       /**每次找到一个最小值，标记，然后修改其相邻的值*/  
-       for(int i=0;i<total;i++)  
-       {  
-           int min=MAX;  
-           int index=-1;  
-           for(int j=0;j<total;j++)  
-           {  
-               if(mark[j]==0&&dis[j]<min)  
-               {  
-                   index=j;  
-                   min=matrix[s][j];  
-               }  
-           }  
-           if(index==-1) return;  
-           mark[index]=1;  
-           for(int k=0;k<total;k++)  
-           {  
-               if(mark[k]==0&&dis[index]+matrix[index][k]<dis[k])  
-               {  
-                   dis[k]=dis[index]+matrix[index][k];  
-               }  
-           }  
-          // printDis(0,this.nodes[i],"第"+i+"次");  
-       }  
-   }  
-   private void printDis(int i, String node, String pre) {    
-       /*System.out.print("\n" + pre + "," + node + "," + i + "--->");   
-       for (int t = 0; t < this.dis.length; t++) {   
-           System.out.print(t + ",");   
-       }*/   
-       System.out.print("\n" + pre + i + "--->");    
-       for (int t : this.dis) {    
-           System.out.print(t + ",");    
-       }    
-       System.out.print("\n");    
-   }    
+	public static Queue<Node> que = new LinkedList<Node>();
+	public static int[] fx = {0, 0, 1, -1};
+	public static int[] fy = {1, -1, 0, 0};
+	public static int bfs(int sx, int sy, int ex, int ey, char[][] mz, int m, int n, Map<Character, Integer> keys, boolean[][][] visited) {
+		while(!que.isEmpty()) que.poll();
+		Node tmp = new Node();
+		tmp.x = sx;
+		tmp.y = sy;
+		que.offer(tmp);
+		while(!que.isEmpty()) {
+			Node p = que.poll();
+			if(p.x == ex && p.y == ey) {
+				return p.cnt;
+			}
+			for(int i = 0; i < 4; ++i) {
+				int newx = p.x + fx[i];
+				int newy = p.y + fy[i];
+				if(newx < 0 || newx >= m || newy < 0 || newy >= n) continue;
+				if(mz[newx][newy] == '0') continue;
+				int sta = p.sta;
+				if(mz[p.x][p.y] >= 'a' && mz[p.x][p.y] <= 'z') {
+					sta |= (1<<keys.get(mz[p.x][p.y]));
+				}
+				if(visited[newx][newy][sta]) continue;
+				if(mz[newx][newy] >= 'A' && mz[newx][newy] <= 'Z') {
+					if((sta & (1<<(keys.get((char)(mz[newx][newy] - 'A' + 'a')))))== 0) {
+						continue;
+					}
+				}
+				visited[newx][newy][sta] = true;
+				tmp = new Node();
+				tmp.x = newx;
+				tmp.y = newy; 
+				tmp.cnt = p.cnt + 1;
+				tmp.sta = sta;
+				que.offer(tmp);
+			}
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		int m = scan.nextInt();
 		int n = scan.nextInt();
 		char[][] matrix = new char[m][n];
+		boolean[][][] visited = new boolean[m][n][n*10];
 		scan.nextLine();
-		int startI = 0, startJ = 0;
-		int endI =0, endJ = 0;
-		int aI =0, aJ =0;
-		int AI = 0, AJ =0;
-		
+		int sx=0, sy=0, ex=0, ey=0;
+		Map<Character, Integer> keys = new HashMap<Character, Integer>();
+		int cnt = 0;
 		for(int i =0; i< m; i++){
 			String s = scan.nextLine();
 			for(int j = 0; j< n; j++){
 				char c = s.charAt(j);
 				if(c=='2'){
-					startI = i;
-					startJ = j;
+					sx = i;
+					sy = j;
 				}else if(c=='3'){
-					endI =i;
-					endJ = j;
+					ex =i;
+					ey = j;
 				}else if(c >='a' && c <='z'){
-					aI =i;
-					aJ= j;
-				}else if(c >='A'&& c <='Z'){
-					AI =i;
-					AJ =j;
+					keys.put(c, cnt++);
 				}
 				matrix[i][j]= c;
 			}
 		}
 		for(int i=0; i< m; i++){
-			System.out.println(Arrays.toString(matrix[i]));
+			for(int j =0; j <n; j++){
+				//cnt把钥匙，则对应了2^cnt种状态
+				for(int k =0; k<(1<<cnt); k++){
+					visited[i][j][k] = false;
+				}
+			}
 		}
-		//先到钥匙处，再由钥匙处到大门处，再到出口。
-		//Dijkstra算法求最短路径
-		
-		
+		int res = bfs(sx,sy, ex,ey, matrix,  m,  n, keys, visited);
+		System.out.println(res);
 		scan.close();
 	}
+
 }
