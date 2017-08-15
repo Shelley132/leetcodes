48554fa0a88c6ee70fe6792be0d5d95844c136ef
Shelley132 <liulijuan132@qq.com>
2017-08-15 14:14:03 +0800

    秋招内推

---
+++ neitui/pinduoduo/Interview11.java
@@ -0,0 +1,15 @@
+package pinduoduo;
+
+/**
+ * @author JUANJUAN
+ * @version 2017年8月14日下午8:52:58
+ */
+public class Interview11 {
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+}
--- neitui/pinduoduo/Main1.java
+++ neitui/pinduoduo/Main1.java
@@ -10,37 +10,38 @@ import java.util.Scanner;
 public class Main1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		int max = Integer.MIN_VALUE;
-		int max2 = Integer.MIN_VALUE;
-		int max3 = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
-		int min2 = Integer.MAX_VALUE;
-		int num;
-		for (int i = 0; i < n; i++) {
-			num = scan.nextInt();
-			if (num > max) {
-				max3 = max2;
-				max2 = max;
-				max = num;
-			} else if (num > max2) {
-				max3 = max2;
-				max2 = num;
-			} else if(num > max3){
-				max3 = num;
-			}else if (num < min) {
-				min2 = min;
-				min = num;
-			} else if (num < min2) {
-				min2 = num;
-			}
-		}
-		long pr1 = max2* max3 * max;
-		long pr2 = min * min2 * max;
-		long res = pr1 > pr2 ?pr1 : pr2;
-		
-		System.out.println(res);
-
-		scan.close();
+        int n = scan.nextInt();
+        int max = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int num;
+        for (int i = 0; i < n; i++) {
+            num = scan.nextInt();
+            if (num > max) {
+                max3 = max2;
+                max2 = max;
+                max = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if(num > max3){
+                max3 = num;
+            }
+            if (num < min) {
+                min2 = min;
+                min = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+        long pr1 = max2* max3;
+        long pr2 = min * min2;
+        // long res = pr1 > pr2 ?pr1 : pr2;
+         
+        System.out.println(pr1 > pr2 ?pr1*max : pr2*max);
+ 
+        scan.close();
 	}
 }
