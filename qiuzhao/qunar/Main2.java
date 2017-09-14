76da932bc6e361f842c8211ac4036e99caa1aad3
Shelley132 <liulijuan132@qq.com>
2017-09-14 13:15:12 +0800

    update

---
+++ qiuzhao/qunar/Main2.java
@@ -0,0 +1,21 @@
+package qunar;
+
+import java.util.Scanner;
+
+/**
+ * @author JUANJUAN
+ * @version 2017年9月14日上午9:53:28
+*/
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		
+		scan.close();
+	}	
+
+}
---
+++ qiuzhao/qunar/Main3.java
@@ -0,0 +1,21 @@
+package qunar;
+
+import java.util.Scanner;
+
+/**
+ * @author JUANJUAN
+ * @version 2017年9月14日上午10:23:28
+*/
+public class Main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println(4);
+		
+		
+		
+		scan.close();
+	}	
+
+}
